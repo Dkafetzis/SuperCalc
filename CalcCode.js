315042c9637d760f178ad8c1bc77bf0eb3480ba0
akatatsu27 <43857160+akatatsu27@users.noreply.github.com>
2018-10-04 22:51:47 +0300

    making the + operation

--- CalcCode.js
+++ CalcCode.js
@@ -1,15 +1,30 @@
 let result=""
-
-
+let storedValue=0
+let operationFlag=0
+document.getElementById("output").innerHTML="0"
 function writeToOutput(x,y) {
     switch(x){
         case 1:
-            result=result+"1"
-            document.getElementById("output").innerHTML=result;
-            break;
+            if(operationFlag==0){
+                result=result+y
+                document.getElementById("output").innerHTML=result;
+            }
+            else if(operationFlag==1){
+                result=y
+                operationFlag=0
+            }
+        break;
         case 2:
+            storedValue=storedValue+Number(result)
+            operationFlag=1
+    
+            break;
+    
+    
+    
+    
+    
     }
-
 
 
 
