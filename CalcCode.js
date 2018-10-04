68e2d1719bf7908589dfdb11ee5970bf6cdbec76
Dkafetzis <didia68@gmail.com>
2018-10-04 23:05:30 +0300

    Working Addition

--- CalcCode.js
+++ CalcCode.js
@@ -1,24 +1,18 @@
 let result=""
 let storedValue=0
 let operationFlag=0
-document.getElementById("output").innerHTML="0"
 function writeToOutput(x,y) {
     switch(x){
         case 1:
-            if(operationFlag==0){
                 result=result+y
-                document.getElementById("output").innerHTML=result;
-            }
-            else if(operationFlag==1){
-                result=y
-                operationFlag=0
-            }
+                document.getElementById("Num").innerHTML=result;
         break;
         case 2:
-            storedValue=storedValue+Number(result)
-            operationFlag=1
-    
-            break;
+            storedValue=storedValue+Number(result);
+            result = "";
+            document.getElementById("Num").innerHTML=result;
+            document.getElementById("Res").innerHTML=storedValue;
+        break;
     
     
     
