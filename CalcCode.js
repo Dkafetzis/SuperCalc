c8c6140ad3cdfc78451c374a8ca0b704db095018
akatatsu27 <43857160+akatatsu27@users.noreply.github.com>
2018-10-04 21:42:37 +0300

    tried to add button functionality :(

---
+++ CalcCode.js
@@ -0,0 +1,15 @@
+let result=""
+
+
+function writeToOutput(x){
+    switch
+        case 1:
+            result=result+1
+            document..getElementById("output").innerHTML=result
+            break;
+
+
+
+
+
+}
