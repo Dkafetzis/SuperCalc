15119f1f4feda6d8bde18146b0abd902570b4bee
Dkafetzis <didia68@gmail.com>
2018-10-15 09:52:33 +0300

    added 2 debug headers, check operand, if this doesn't  work restart

--- CalcCode.js
+++ CalcCode.js
@@ -4,34 +4,43 @@ let storedValue1=0;
 let operand=0;
 let pressedOperandButton=0;
 let Num = 0;
+function show(){
+    document.getElementById("debug0").innerHTML= "storedValue =" + storedValue + " " + "storedValue1 =" + storedValue1 + " " + "result =" + result + " " + "operand =" + operand;
+    document.getElementById("debug1").innerHTML= "pressedOperandButton =" + pressedOperandButton + " " + "operand =" + operand + "Num =" + Num;
+}
 function calculate(z){
     if(pressedOperandButton==0){
         switch(z){
             case 0:
                 storedValue=Number(result);
                 document.getElementById("res").innerHTML=storedValue;
+                show();
             break;
             case 2:
                 storedValue=storedValue+Number(result);
                 document.getElementById("res").innerHTML=storedValue;
+                show();
             break;
             case 3:
               storedValue=storedValue-Number(result);
                 document.getElementById("res").innerHTML=storedValue;
+                show();
             break;
             case 4:
                 storedValue=storedValue*Number(result);
                 document.getElementById("res").innerHTML=storedValue;
+                show();
             break;
             case 5:
                 Num = Number(result);
-                if(Num==0){
+                if(Num == 0){
                     document.getElementById("res").innerHTML="NaN"; 
                     document.getElementById("output").innerHTML="NaN";
-                }
-                else{
+                    show();
+                }else{
                     storedValue1=storedValue1/Number(result);
                     document.getElementById("res").innerHTML=storedValue1;
+                    show();
                 }
             break;    
         }
@@ -39,77 +48,87 @@ function calculate(z){
 }
 function writeToOutput(x,y) {
     switch(x){
-        // concatinate digit
-        case 1: 
+        case 1:  // concatinate digit 
             result=result + y.toString();
             document.getElementById("output").innerHTML=result;
-            pressedOperandButton=0;  
+            pressedOperandButton=0;
+            show();  
         break;
-        case 2:
-        // prepare +
+
+        case 2:  // prepare +
             calculate(operand);
-            temp=result;
+            temp=Number(result);
             result="";
             if(pressedOperandButton==0){
                 document.getElementById("output").innerHTML=temp;
+                show();
             }
             operand=2;
              pressedOperandButton=1;
+             show();
 
         break;
-        // prepare -
-        case 3:
+
+        case 3:  // prepare -
             calculate(operand);
-            temp=result;
+            temp=Number(result);
             result="";
             if(pressedOperandButton==0){
                 document.getElementById("output").innerHTML=temp;
+                show();
             }
             operand=3;
             pressedOperandButton=1;
+            show();
         break;
-        // prepare *
-        case 4:
+
+        case 4:  // prepare *
             calculate(operand)
-            temp=result;
+            temp=Number(result);
             result="";
             if(pressedOperandButton==0){
                 document.getElementById("output").innerHTML=temp;
+                show();
             }
             operand=4;
             pressedOperandButton=1;
+            show();
         break;
-        // prepare /    
-        case 5:
+
+        case 5:  // prepare /
             calculate(operand);
-            temp=result;
+            temp=Number(result);
             result="";
             if(pressedOperandButton==0){
                 document.getElementById("output").innerHTML=temp;
+                show();
             }
             operant=5;
             pressedOperandButton=1;
+            show();
         break;
-        // =
-        case 6:
+
+        case 6:  // =
             if(pressedOperandButton==0){
                 calculate(operand);
                 result=storedValue;
                 document.getElementById("output").innerHTML=storedValue;
                 pressedOperandButton=1;
                 operand=0;
+                show();
             }
         break;
-        // reset
-        case 7:
+        
+        case 7:  // reset
             storedValue=0;
             result="";
             operand=0;
             document.getElementById("output").innerHTML="0";
             document.getElementById("res").innerHTML="0";
             pressedOperandButton=0;
-        break;
-    }
+            show();
+        break; 
+}
 
 
 
